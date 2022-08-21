8c492a40a2a862b1534f6353b6806f0c3c88c79a
Ehsan Jalali <JMSEhsan@gmail.com>
2022-08-20 22:59:01 -0600

    first commit

---
+++ JavaScriptpractice.js
@@ -0,0 +1,12 @@
+//# Python vs. JavaScript for Development, LinkedIn Course
+let var1 = 50;
+const piNum = 3.1415;
+console.log('var1 = '+ var1+'\n'+'piNum = '+ piNum);
+
+pizzaToppings =["pepperoni", "mushrooms", "pineapple"];
+for(let topping of pizzaToppings){
+    console.log(topping);
+}
+
+
+
---
+++ pythonpractice.py
@@ -0,0 +1,8 @@
+# Python vs. JavaScript for Development, LinkedIn Course
+var1 = 60
+print('var1= ', var1)
+
+pizzaToppings =["pepperoni", "mushrooms", "pineapple"]
+for topping in pizzaToppings:
+    print(topping)
+
